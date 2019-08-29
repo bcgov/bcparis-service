ce1a0ab3d520b741bccbc55e30a0ed01c4241e03
Marcos Agostta <marcos.agostta@gmail.com>
2019-08-29 14:33:19 -0300

    Update Message object

--- src/test/java/ca/bc/gov/iamp/bcparis/transformation/MessageTransformTest.java
+++ src/test/java/ca/bc/gov/iamp/bcparis/transformation/MessageTransformTest.java
@@ -28,26 +28,22 @@ public class MessageTransformTest {
 		Assert.assertEquals("HC BC40940", list.get(7));
 		Assert.assertEquals("BC41027", list.get(8));
 		Assert.assertEquals(EMPTY_LINE, list.get(9));
-		Assert.assertEquals("SNME:WISKIN", list.get(10));
-		Assert.assertEquals("G1:TOMAS", list.get(11));
-		Assert.assertEquals("G2:GEORGE", list.get(12));
-		Assert.assertEquals("G3:ALPHONSE", list.get(13));
-		Assert.assertEquals("DOB:20020325", list.get(14));
-		Assert.assertEquals(EMPTY_LINE, list.get(15));
-		Assert.assertEquals("2019051520054820190515200548", list.get(16));
+		Assert.assertEquals("SNME:WISKIN/G1:TOMAS/G2:GEORGE/G3:ALPHONSE/DOB:20020325", list.get(10));
+		Assert.assertEquals(EMPTY_LINE, list.get(11));
+		Assert.assertEquals("2019051520054820190515200548", list.get(12));
 	}
 	
 	@Test
 	public void extract_CDATA_escaped_success() {
-		String message = "G3:ALPHONSE/DOB:20020325\n\n2019051520054820190515200548";
+		String message = "FROM:BC41127SNME:WISKIN/DOB:20020325\n\n2019051520054820190515200548";
 		
 		List<String> list = MessageTransform.extractCDATAAttributes(message);
 		
 		System.out.println("\nExtract 2");
 		list.forEach( attr ->System.out.println("attribute= " + attr));
 		
-		Assert.assertEquals("G3:ALPHONSE", list.get(0));
-		Assert.assertEquals("DOB:20020325", list.get(1));
+		Assert.assertEquals("FROM:BC41127", list.get(0));
+		Assert.assertEquals("SNME:WISKIN/DOB:20020325", list.get(1));
 		Assert.assertEquals(EMPTY_LINE, list.get(2));
 		Assert.assertEquals("2019051520054820190515200548", list.get(3));
 	}

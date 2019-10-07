225e24ee26ad9f2cd148346076f6ca26d0fc127a
Marcos Agostta <marcos.agostta@gmail.com>
2019-10-07 14:07:09 -0300

    Fix RNS ICBC query

--- src/main/java/ca/bc/gov/iamp/bcparis/processor/datagram/VehicleProcessor.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/processor/datagram/VehicleProcessor.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import ca.bc.gov.iamp.bcparis.model.message.Layer7Message;
 import ca.bc.gov.iamp.bcparis.model.message.body.Body;
@@ -67,13 +68,16 @@ public class VehicleProcessor implements DatagramProcessor{
 		for(String query : queryParams) {
 			final String transaction = getTransaction(query);
 			
+			if(query.startsWith("RNS"))
+				query = getRNSQuery(query);
+			
 			// Remove /h /H
 			if(query.endsWith("/h") || query.endsWith("/H"))
 				query = query.substring(0, query.length() - 2);
 			
 			// Add date and time
 			query += "/" + getLocalTimeNowICBCFormat();
-				
+			
 			final String imsContent = icbcPayload
 					.replace("${transactionName}", transaction)
 					.replace("${fromORI}", from)
@@ -84,6 +88,47 @@ public class VehicleProcessor implements DatagramProcessor{
 		}
 		
 		return result;
+	}
+	
+	/**
+	 * @param query
+	 * 		RNS:CAMERON/G1:JESSA/G2:/DOB:19890926/RSVP:16
+	 * @return
+	 * 		RNS:CAMERON/JESSA//1989-09-26/RSVP:16
+	 */
+	private String getRNSQuery(final String query) {
+		final String surname 	= extractFromQuery(query, "RNS:");
+		final String g1 		= extractFromQuery(query, "G1:");
+		final String g2 		= extractFromQuery(query, "G2:");
+		final String dob 		= formatDate(extractFromQuery(query, "DOB:"));
+		final String rsvp 		= extractFromQuery(query, "RSVP:");
+		
+		String rnsQuery = String.format("RNS:%s/%s/%s/%s", surname, g1, g2, dob);
+		
+		if(!StringUtils.isEmpty(rsvp))
+			rnsQuery += "/RSVP:" + rsvp;
+		
+		return rnsQuery;
+	}
+	
+	private String formatDate(String date) {
+		if(!StringUtils.isEmpty(date)) {
+			return String.format("%s-%s-%s", date.substring(0, 4), date.substring(4, 6),  date.substring(6, 8));
+		}
+		return "";
+	}
+	
+	private String extractFromQuery(final String query, final String attribute) {
+		final int indexAttr = query.indexOf(attribute);
+		if(indexAttr != -1) {
+			final int indexEnd = query.indexOf("/", indexAttr);
+			final int attrLength = attribute.length();
+			if(indexEnd != -1)
+				return query.substring(indexAttr + attrLength, indexEnd);
+			else
+				return query.substring(indexAttr + attrLength);
+		}
+		return "";
 	}
 	
 	private String getTransaction(String query) {

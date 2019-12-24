a9e35c500e403858052b8184ac7adc3975df57a8
alex <alex.joyeux@gov.bc.ca>
2019-12-24 09:52:19 -0800

    added known keys to constants

--- src/main/java/ca/bc/gov/iamp/bcparis/message/MessageUtils.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/message/MessageUtils.java
@@ -10,48 +10,48 @@ public class MessageUtils {
 
     public static final String SEMICOLLON = ":";
 
+
     public static String GetValue(String message, String key) {
 
         HashSet<String> knownTokens = new HashSet<String>() {{
-            add("SN");
-            add("MT");
-            add("MSID");
+            add(Keys.RESPONSE_SCHEMA_SN_KEY);
+            add(Keys.RESPONSE_SCHEMA_MT_KEY);
+            add(Keys.RESPONSE_SCHEMA_MSID_KEY);
             add(Keys.RESPONSE_SCHEMA_FROM_KEY);
             add(Keys.RESPONSE_SCHEMA_TO_KEY);
-            add("SUBJ");
+            add(Keys.RESPONSE_SCHEMA_SUBJ_KEY);
             add(Keys.RESPONSE_SCHEMA_TEXT_KEY);
             add(Keys.RESPONSE_SCHEMA_RE_KEY);
-            add("SNME");
-            add("DL");
-            add("LIC");
-            add("ODN");
-            add("FLC");
-            add("VIN");
-            add("REG");
-            add("RNS");
-            add("RVL");
-            // TODO: REMOVE TOKEN AFTER SATELLITE SERVICE IS DEPRECATED
-            add("TestRNS");
+            add(Keys.RESPONSE_SCHEMA_SNME_KEY);
+            add(Keys.RESPONSE_SCHEMA_DL_KEY);
+            add(Keys.RESPONSE_SCHEMA_LIC_KEY);
+            add(Keys.RESPONSE_SCHEMA_ODN_KEY);
+            add(Keys.RESPONSE_SCHEMA_FLC_KEY);
+            add(Keys.RESPONSE_SCHEMA_VIN_KEY);
+            add(Keys.RESPONSE_SCHEMA_REG_KEY);
+            add(Keys.RESPONSE_SCHEMA_RNS_KEY);
+            add(Keys.RESPONSE_SCHEMA_RVL_KEY);
+            add(Keys.RESPONSE_SCHEMA_TEST_RNS_KEY);
         }};
 
-        if(knownTokens.contains(key)) {
+        if (knownTokens.contains(key)) {
             knownTokens.remove(key);
         } else {
-            throw new  IllegalArgumentException("Key is not a known token.");
+            throw new IllegalArgumentException("Key is not a known token.");
         }
 
-        if(StringUtils.isEmpty(message)) return "";
+        if (StringUtils.isEmpty(message)) return "";
 
         int startIndex = message.indexOf(key + SEMICOLLON) + key.length() + 1;
         int currentEndIndex = message.length();
 
-        if(startIndex == -1) return "";
+        if (startIndex == -1) return "";
 
-        for(String token: knownTokens) {
+        for (String token : knownTokens) {
 
             int tokenIndex = message.indexOf(token + SEMICOLLON);
 
-            if(tokenIndex < currentEndIndex && tokenIndex >= startIndex) {
+            if (tokenIndex < currentEndIndex && tokenIndex >= startIndex) {
                 currentEndIndex = tokenIndex;
             }
         }

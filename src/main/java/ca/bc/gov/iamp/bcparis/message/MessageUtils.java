c47c78f2efe7684deae7ca5b4c8c7049cfcc6b55
alex <alex.joyeux@gov.bc.ca>
2019-12-24 08:25:27 -0800

    Added new MessageUtils class to parse string messages

---
+++ src/main/java/ca/bc/gov/iamp/bcparis/message/MessageUtils.java
@@ -0,0 +1,9 @@
+package ca.bc.gov.iamp.bcparis.message;
+
+public class MessageUtils {
+
+    public static String GetValue(String message, String key) {
+        return "value";
+    }
+
+}
---
+++ src/test/java/ca/bc/gov/iamp/bcparis/message/MessageUtilsGetValueTest.java
@@ -0,0 +1,51 @@
+package ca.bc.gov.iamp.bcparis.message;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class MessageUtilsGetValueTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {
+                        "HC  BC41027]\\\"BC41127]\\\"RE SNME:SMITH/G1:JANE/G2:MARY/DOB:19000101/SEX:F]\\\"BCDL: 7088384       STATUS: NORMAL            PRIMARY DL STATUS:   NONE]\\\"SMITH, JUDY MADELINE                          LEARNER DL STATUS:   NONE]\\\"M/A BOX 195                                   TEMPORARY DL STATUS: NONE]\\\"DUNCAN BC]\\\"5175 TZOUHALEM RD                             LICENCE TYPE:CLIENT STUB]\\\"V9L 3X3]\\\"                                                OTHER JUR DL:]\\",
+                        new String[][]{
+                                {"SNME", "SMITH/G1:JANE/G2:MARY/DOB:19000101/SEX:F]\\\""},
+                                {"BCDL", " 7088384       "}}
+                }
+        });
+    }
+
+    public String input;
+    public String[][] expected;
+
+    public MessageUtilsGetValueTest(String input, String[][] expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Test
+    public void execute() {
+
+        for(int i = 0; i < expected.length; i++) {
+
+            String result = MessageUtils.GetValue(this.input, expected[i][0]);
+
+            Assert.assertEquals(
+                    "Attribute Value different from input",
+                    expected[i][1],
+                    result);
+        }
+
+    }
+
+
+}

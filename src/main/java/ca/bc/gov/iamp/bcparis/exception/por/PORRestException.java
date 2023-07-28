59da0ca1d155c2a34aad93885dec307a8191a1e6
nikhilmaddi <nikhil.maddikunta@gov.ab.ca>
2023-07-28 11:53:39 -0700

    changes in the exception

--- src/main/java/ca/bc/gov/iamp/bcparis/exception/por/PORRestException.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/exception/por/PORRestException.java
@@ -5,7 +5,7 @@ public class PORRestException extends RuntimeException{
 
 	private static final long serialVersionUID = 217462322973509320L;
 
-	private static final String BASE_MESSAGE = "Exception to call POR Rest Service";
+	private static final String BASE_MESSAGE = "Exception to call #POR Rest service #";
 	private String responseContent;
 	
 	public PORRestException(String secondLineMessage, String responseContent, Throwable cause) {

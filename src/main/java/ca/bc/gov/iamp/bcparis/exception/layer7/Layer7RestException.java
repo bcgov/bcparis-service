6daebf626bbf3f2b14e310fcfd69df3de1ae7843
nikhilmaddi <nikhil.maddikunta@gov.ab.ca>
2023-08-01 13:01:49 -0700

    changes in the exception

--- src/main/java/ca/bc/gov/iamp/bcparis/exception/layer7/Layer7RestException.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/exception/layer7/Layer7RestException.java
@@ -1,15 +1,22 @@
 package ca.bc.gov.iamp.bcparis.exception.layer7;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.bc.gov.iamp.bcparis.exception.icbc.ICBCRestException;
+
 public class Layer7RestException extends RuntimeException {
 
 	private static final long serialVersionUID = 3151843112893690400L;
 
-	private static final String BASE_MESSAGE = " Exception to call #Layer 7 REST service#";
+	private static final String BASE_MESSAGE = "Exception to call #Layer 7 REST service#.";
 	private String responseContent;
+	private final Logger log = LoggerFactory.getLogger(Layer7RestException.class);
 	
 	public Layer7RestException(String secondLineMessage, String responseContent, Throwable cause) {
 		super(BASE_MESSAGE + "\n" + secondLineMessage, cause);
 		this.responseContent = responseContent;
+		log.error(BASE_MESSAGE);
 	}
 	
 	public String getResponseContent() {
--- src/main/java/ca/bc/gov/iamp/bcparis/exception/por/PORRestException.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/exception/por/PORRestException.java
@@ -1,5 +1,9 @@
 package ca.bc.gov.iamp.bcparis.exception.por;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.bc.gov.iamp.bcparis.exception.layer7.Layer7RestException;
 
 public class PORRestException extends RuntimeException{
 
@@ -7,10 +11,12 @@ public class PORRestException extends RuntimeException{
 
 	private static final String BASE_MESSAGE = "Exception to call #POR Rest service #";
 	private String responseContent;
+	private final Logger log = LoggerFactory.getLogger(PORRestException.class);
 	
 	public PORRestException(String secondLineMessage, String responseContent, Throwable cause) {
 		super(BASE_MESSAGE + "\n" + secondLineMessage, cause);
 		this.responseContent = responseContent;
+		log.error(BASE_MESSAGE);
 	}
 	
 	public PORRestException(String secondLineMessage, String responseContent) {

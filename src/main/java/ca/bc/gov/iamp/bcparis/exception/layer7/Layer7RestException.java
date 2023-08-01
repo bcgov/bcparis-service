66663e347fb3754e37f0bdf802ef75df64394373
NikhilMM89 <117143852+NikhilMM89@users.noreply.github.com>
2023-07-31 21:47:38 -0700

    Update Layer7RestException.java

--- src/main/java/ca/bc/gov/iamp/bcparis/exception/layer7/Layer7RestException.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/exception/layer7/Layer7RestException.java
@@ -4,7 +4,7 @@ public class Layer7RestException extends RuntimeException {
 
 	private static final long serialVersionUID = 3151843112893690400L;
 
-	private static final String BASE_MESSAGE = "Exception to post to Layer 7 Rest Service.";
+	private static final String BASE_MESSAGE = "Exception to call #Layer 7 REST service#.";
 	private String responseContent;
 	
 	public Layer7RestException(String secondLineMessage, String responseContent, Throwable cause) {

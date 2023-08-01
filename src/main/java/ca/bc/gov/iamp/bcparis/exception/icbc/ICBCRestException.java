0e94198d9d771b3cd00c3c7846ff1def6ae1f198
NikhilMM89 <117143852+NikhilMM89@users.noreply.github.com>
2023-07-31 21:47:06 -0700

    Update ICBCRestException.java

--- src/main/java/ca/bc/gov/iamp/bcparis/exception/icbc/ICBCRestException.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/exception/icbc/ICBCRestException.java
@@ -5,7 +5,7 @@ public class ICBCRestException extends RuntimeException {
 
 	private static final long serialVersionUID = -6290556125571489942L;
 	
-	private static final String BASE_MESSAGE = "Exception to call ICBC Rest Service.";
+	private static final String BASE_MESSAGE = " Exception to call #ICBC Rest Service#.";
 	private String responseContent;
 
 	public ICBCRestException(String secondLineMessage, String responseContent, Throwable cause) {

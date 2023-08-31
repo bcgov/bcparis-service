d1fafa17479b3e94dc51bd075a17733f5e2123db
NikhilMM89 <117143852+NikhilMM89@users.noreply.github.com>
2023-08-31 10:38:49 -0700

    Enabling HTTP Strict Transport Security in response headers

---
+++ src/main/java/ca/bc/gov/iamp/bcparis/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package ca.bc.gov.iamp.bcparis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+	  http
+	  .headers()
+		.httpStrictTransportSecurity()
+			.includeSubdomains(true)
+			.maxAgeSeconds(31536000);
+  }
+}

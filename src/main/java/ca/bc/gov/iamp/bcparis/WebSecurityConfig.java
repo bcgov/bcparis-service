e48dc924ea5de1c3362b91282e98aa037957af2e
NikhilMM89 <117143852+NikhilMM89@users.noreply.github.com>
2023-09-26 09:58:19 -0700

    Update WebSecurityConfig.java

--- src/main/java/ca/bc/gov/iamp/bcparis/WebSecurityConfig.java
+++ src/main/java/ca/bc/gov/iamp/bcparis/WebSecurityConfig.java
@@ -1,19 +1,1 @@
-package ca.bc.gov.iamp.bcparis;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-	  http
-	  .headers()
-		.httpStrictTransportSecurity()
-			.includeSubDomains(true)
-			.maxAgeInSeconds(31536000);
-  }
-}
